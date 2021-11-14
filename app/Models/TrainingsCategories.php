449174b006562b71043f109bea84d728e654f059
gturyz <gturyz@gmail.com>
2021-11-14 22:34:44 +0000

    fix class name

--- app/Models/TrainingsCategories.php
+++ app/Models/TrainingsCategories.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CategoriesTrainings extends Model
+class TrainingsCategories extends Model
 {
     use HasFactory;
 
